0c9b09ff234e2d3467b027d99a93bc8766db7e04
ACER <ACER@DESKTOP-1BR1UTS>
2022-01-10 14:35:11 +0530

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Anil", "Sharma", "Abdullah",
+                       "Imran", "Siva", "Naresh",
+                       "Suresh", "Suman" };
+            //UsingAmonymusMethodsQueries
+            UsingAnonymousMethods(names);
+        }
+        private static void UsingAnonymousMethods(string[] names)
+        {
+            Func<string, bool> filter = delegate (string s)
+            {
+                return s.Length == 5;
+            };
+            Func<string, string> extarct = delegate (string s)
+            {
+                return s;
+            };
+            Func<string, string> project = delegate (string s)
+            {
+                return s.ToUpper();
+            };
+
+            IEnumerable<string> query = names.Where(filter)
+                .OrderBy(extarct)
+                .Select(project);
+
+            foreach (string item in query)
+                Console.WriteLine(item);
+            Console.Read();
+        }
+    }
+}
